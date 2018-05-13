006f9c993efa198150230b55626c78c9b283a5c8
gwitwer <gdavid.witwer@gmail.com>
2018-05-13 13:15:54 -0400

    update Shop db

--- server/models/shop.js
+++ server/models/shop.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const shopSchema = new Schema({
-  email: { type: 'String', required: true, uniue: true },
-  pass: { type: 'String', required: true },
-  type: { type: 'String', default: 'player', required: true }, // shop, admin, coach, player
-  cuid: { type: 'String', required: true },
-  dateCreated: { type: 'Date', default: Date.now, required: true },
+	access_token: { type: String },
+	charge_id: { type: Number },
+	dateCreated: { type: Number },
+	email: { type: String },
+	name: { type: String, required: true, unique: true },
 });
 
 export default mongoose.model('Shop', shopSchema);
