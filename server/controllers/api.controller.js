00660ca0921688625c59e4c9a0719089cae13a37
gwitwer <gdavid.witwer@gmail.com>
2018-05-14 13:47:50 -0400

    additional coupon data

--- server/controllers/api.controller.js
+++ server/controllers/api.controller.js
@@ -10,15 +10,21 @@ export function postSubscribe(req, res) {
 export function getVerifyCoupon(req, res) {
   console.log(req.params);
   const id = req.params.coupon;
-  let valid = false;
+  let response = {
+    valid: false,
+    percent_off: 0,
+    amt_off: 0,
+  };
   stripe.coupons.list((err, coupons) => {
     console.log(coupons);
     coupons.data.forEach(coupon => {
-      if (coupon.id === id && coupon.valid) {
-        valid = true;
+      if (coupon.id === id) {
+        Object.keys(response).forEach(k => {
+          response[k] = coupons[k];
+        });
       }
     });
 
-    res.json({ valid });
+    res.json(response);
   });
 }
